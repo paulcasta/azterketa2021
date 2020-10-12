84a6403346ec336ecd42ee6d5b9e4d11f646f336
34636 <ibaifranco@gmail.com>
2020-10-12 17:38:17 +0200

    kautoketa simple bat (pasahitza eskutatuta)

--- src/main/java/ehu/isad/Main.java
+++ src/main/java/ehu/isad/Main.java
@@ -1,6 +1,9 @@
 package ehu.isad;
 
+import ehu.isad.controllers.Controller;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,11 +14,17 @@ public class Main extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception{
 
-    Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample.fxml"));
+    Parent root = (Parent) loader.load();
+
+    Controller controller = loader.getController();
+    //controller.setMainApp(this);
+
     primaryStage.setTitle("Hello World");
     primaryStage.setScene(new Scene(root, 300, 275));
     primaryStage.show();
   }
+
 
 
   public static void main(String[] args) {
--- src/main/java/ehu/isad/controllers/Controller.java
+++ src/main/java/ehu/isad/controllers/Controller.java
@@ -1,4 +1,26 @@
 package ehu.isad.controllers;
 
+import ehu.isad.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField txtErabiltzaile;
+
+    @FXML
+    private PasswordField txtPasahitza;
+
+    @FXML
+    private Button kautotuBotoia;
+
+    @FXML
+    void onClick(ActionEvent event) {
+        System.out.println(txtErabiltzaile.getText() + ":" + txtPasahitza.getText());
+    }
+
+
 }
